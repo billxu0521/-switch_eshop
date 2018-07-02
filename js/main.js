c6e9b9e483cb820c7c9ff26944e754db31f4eaad
billxu <billxu0521@gmail.com>
2018-07-02 12:15:09 +0800

    調整版型 更改一些敘述

--- js/main.js
+++ js/main.js
@@ -49,14 +49,12 @@
 
     //讀取全部資料
     function eshop_game_query(){
-        
       var sheetID = "1dt6FX7yA_g95dX1-5vQg-_GVpfcM3PPNNs645W1YrE4", // 試算表代號
       gid = "735367841", // 工作表代號
       sql = "SELECT A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN "; // SQL 語法
       //各地區中文 用硬幹的
       //var country = ['AT','AU', 'BE','BG','CA','CH','CY','CZ','DE','DK','EE','ES','FI','FR','GB','GR','HR','HU','IE','IT','JP','LT','LU' ,'LV' ,'MT','MX','NL' ,'NO','NZ', 'PL' ,'PT','RO','RU','SE','SI','SK' ,'US' ,'ZA'];
       var country = ['AUS','AUT','BEL','BGR','CAN','HRV','CYP','CZE','DNK','EST','FIN','FRA','DEU','GRC','HUN','IRL','ITA','JPN','LVA','LTU','LUX','MLT','MEX','NLD','NZL','NOR','POL','PRT','ROU','RUS','SVK','SVN','ZAF','ESP','SWE','CHE','GBR','USA'];                   
-      
       var c_country = {
         'AUT':'奧地利',
         'AUS':'澳大利亞',
@@ -133,23 +131,19 @@
           var show_onsale_price = '';
           for(a = 0; a < country.length; a++){
             //console.log(dataGroup.length);
-            
             if(dataGroup[a+1] === null) {
               price[country[a]] = 0;
             }else if(dataGroup[a+1].v === null){
               price[country[a]] = 0;
             }else{
-              
               price[country[a]] = dataGroup[a+1].v;
 
             }
-          }  
-          
+          }
           var sortedPrice=sortProperties(price);
           var best_price = 0;
           for(x = 0; x < sortedPrice.length ; x ++){
             var high_price = sortedPrice[sortedPrice.length-1][1].toFixed(0);
-           
             if(sortedPrice[x][1].toFixed(1) > 0){
               //console.log(game_name+"//"+sortedPrice[x][1]);
             best_price = sortedPrice[x][1].toFixed(0); 
@@ -163,15 +157,12 @@
                 show_price += "<div id='box'><div id='higher_box' higher_price='"+ high_price +"''><a id='price_title'>最高價格</a><div id='box_hr' ></div><a id='higher_contry'>"+c_country[sortedPrice[sortedPrice.length-1][0]]+"</a><a id='higher_price'> NT$" + high_price + "</a><br></div></div>";
                 show_onsale_price +=  "<div id='onsale_box'>與最高價地區價差高達<a id='pricetip'>NT$" + (high_price - best_price).toFixed(0) + "</a>!<br></div>";
               }
-
             }
               break;
             }else{
               show_price = "    <div id='area_box'>---<br></div>";
             }
-
           }
-          
           html += "<div style='position:relative;'><div class='game_box' name='"+ game_name.toUpperCase() + game_name.toLowerCase() + "_" + game_name + "_"+ game_cname + "'>" + "<div id='pricebox'><div id='showname'><a id='c_name'>" + game_cname + "</a><br><a href='https://www.google.com.tw/search?q="+ game_name +" switch' target='_blank' id='name'>" + game_name + "</a></div>" + show_onsale_price +show_price +  "</div></div></div>";
           }
         $("#game_count").html('目前遊戲數量:'+game_count); 
@@ -196,26 +187,19 @@
           $(".game_box").hide();
           $("[name*='" +  $(selector).val() + "']").show();
         }
-
       });
     }
 
     //價格篩選器
     function search_price(){
       $('.price').on('keyup',function(){
-        console.log('min:' + $('#min_price').val());
-        console.log('max:' + $('#max_price').val());
-
         var _obj = $(".game_box");
-        //console.log(_obj.children("#pricebox").children("#box").children("#lower_box").attr("lower_price"));
         $(".game_box").hide();
         _obj.each(function( index ) {
-          //console.log($( this ).children("#pricebox").children("#box").children("#lower_box").attr("lower_price"));
           var _price = $( this ).children("#pricebox").children("#box").children("#lower_box").attr("lower_price");
           if($('#min_price').val()=='' || $('#max_price').val()==''){ 
             $(".game_box").show();
           }else if($('#min_price').val() < parseInt(_price) && $('#max_price').val() > parseInt(_price)){
-            //console.log($(this));
             $(this).show();
           }
         });
@@ -229,10 +213,8 @@
       for(var key in obj)
         if(obj.hasOwnProperty(key))
           sortable.push([key, obj[key]]); 
-
-      sortable.sort(function(a, b)
-      {
-        return a[1]-b[1]; 
-      });
-      return sortable; 
+          sortable.sort(function(a, b){
+            return a[1]-b[1]; 
+          });
+        return sortable; 
     }
